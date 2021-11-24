bf170b993a1876a26253a7fda706a184e3288efa
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2021-11-24 14:57:59 +0530

    Update sdk_test.go

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestMyFunction(t *testing.T) {
 	input := "Rohith"
-	expected := "Hello Rohith"
+	expected := "Hello !Rohith"
 	actual := MyFunction(input)
 
 	if expected != actual {
