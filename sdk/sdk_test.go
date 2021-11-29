86b9bc90eb90eaf1685e37e3d51c11d344d61c38
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2021-11-29 21:03:39 +0530

    Update sdk_test.go

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -6,6 +6,11 @@ func TestMyFunction(t *testing.T) {
 	input := "Rohith"
 	expected := "Hello Rohith"
 	actual := MyFunction(input)
+	
+	env_varaiable  = os.Getenv("MY_SECRET")
+	if env_varaiable !="NEW"{
+		t.Fatal(env_varaiable)
+	}
 
 	if expected != actual {
 		t.Fatal("Data Mismatched")
