ed90b7cb035a19807b764e050373bd4da9f619d3
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2021-11-30 09:46:46 +0530

    new updated

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -11,7 +11,7 @@ func TestMyFunction(t *testing.T) {
 	actual := MyFunction(input)
 	
 	env_varaiable  := os.Getenv("MY_SECRET")
-	if env_varaiable !="ROHITH"{
+	if env_varaiable !="ROHIT"{
 		t.Fatal(env_varaiable)
 	}
 
