7b36e5b2fa035b8bb443c560eba30545cd0b2416
AntonyRohithAkash <antonyrohtih.akash@team.neustar>
2021-12-06 11:20:24 +0530

    Phase 10

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -3,11 +3,11 @@ package sdk
 import "fmt"
 
 type SDK struct {
-	ID   int
+	Id   int
 	Name string
 }
 
 func (sdk *SDK) String() string {
-	return fmt.Sprintf("ID:%v - Name:%v", sdk.ID, sdk.Name)
+	return fmt.Sprintf("ID:%v - Name:%v", sdk.Id, sdk.Name)
 
 }
--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -7,8 +7,8 @@ import (
 
 func TestMyFunction(t *testing.T) {
 	envVariable := os.Getenv("MY_SECRET")
-	expected := &SDK{ID: 1, Name: "ROHITH"}
-	found := &SDK{ID: 1, Name: envVariable}
+	expected := &SDK{Id: 1, Name: "ROHITH"}
+	found := &SDK{Id: 1, Name: envVariable}
 
 	if envVariable != "ROHITH" {
 		t.Fatal(envVariable)
