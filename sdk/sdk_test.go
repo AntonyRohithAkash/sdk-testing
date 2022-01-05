2823640593e2694b192b8496b3ef0b66c1489dc9
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2022-01-05 20:00:48 +0530

    Update sdk_test.go

--- sdk/sdk_test.go
+++ sdk/sdk_test.go
@@ -8,6 +8,7 @@ func TestMyFunction(t *testing.T) {
 	expected := "ID:1 - Name:Rohith"
 	sdk := SDK{ID: 1, Name: "Rohith"}
 	found := sdk.String()
+	t.Fatalf("mismatch")
 	if found != expected {
 		t.Fatalf("mismatch")
 	}
